e9fa844ee63eb37d073676767d227b9395b05cf8
Mario <mjvinti@gmail.com>
2018-03-01 21:47:27 -0500

    Added user registration

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+// const passport = require('passport');
 const router = express.Router();
+
+// Load User Model
+require('../models/User');
+const User = mongoose.model('users');
 
 // User Login Route
 router.get('/login', (req, res) => {
@@ -17,11 +23,11 @@ router.post('/register', (req, res) => {
 	let errors = [];
 
 	if (req.body.password !== req.body.password2) {
-		errors.push({text: 'Passwords do not match'});
+		errors.push({ text: 'Passwords do not match' });
 	}
 
 	if (req.body.password.length < 4) {
-		errors.push({text: 'Password must be at least 4 characters'});
+		errors.push({ text: 'Password must be at least 4 characters' });
 	}
 
 	if (errors.length > 0) {
@@ -33,7 +39,35 @@ router.post('/register', (req, res) => {
 			password2: req.body.password2
 		});
 	} else {
-		res.send('passed');
+		User.findOne({ email: req.body.email })
+			.then(user => {
+				if (user) {
+					req.flash('error_msg', 'Email already registered');
+					res.redirect('/users/register')
+				} else {
+					const newUser = new User({
+						name: req.body.name,
+						email: req.body.email,
+						password: req.body.password
+					});
+
+					bcrypt.genSalt(10, (err, salt) => {
+						bcrypt.hash(newUser.password, salt, (err, hash) => {
+							if (err) throw err;
+							newUser.password = hash;
+							newUser.save()
+								.then(user => {
+									req.flash('success_msg', 'You are now registered and can log in');
+									res.redirect('/users/login');
+								})
+								.catch(err => {
+									console.log(err);
+									return;
+								});
+						});
+					});
+				}
+			})
 	}
 });
 
