e0cc2d8b425de740bbef595b19fe6d5b2ea845fd
Mario <mjvinti@gmail.com>
2018-03-01 20:43:05 -0500

    Added login and register views

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 
-// User Register Route
-router.get('/register', (req, res) => {
-	res.send('register');
-});
-  
 // User Login Route
 router.get('/login', (req, res) => {
-	res.send('login');
+	res.render('users/login');
+});
+
+// User Register Route
+router.get('/register', (req, res) => {
+	res.render('users/register');
 });
 
 module.exports = router;
