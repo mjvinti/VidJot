56117b8cd2bafc143f4a17efff39c01f619b2955
Mario <mjvinti@gmail.com>
2018-03-02 11:05:22 -0500

    Fixed misspelling

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = {mongoURI: 'mongodb://mario:p@$$w0rd@ds155268.mlab.com:55268/vidjot-prod-db'}
+  module.exports = {mongoURI: 'mongodb://mario:password@ds155268.mlab.com:55268/vidjot-prod-db'}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
