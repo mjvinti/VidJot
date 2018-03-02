dc4ea0fe6f33100504166063bfc28248525760bf
Mario <mjvinti@gmail.com>
2018-03-02 09:06:03 -0500

    Hooked up local strategy

---
+++ config/passport.js
@@ -0,0 +1,12 @@
+const localStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+// Load User Model
+const User = mongoose.model('users');
+
+module.exports = function(passport) {
+  passport.use(new localStrategy({usernameField: 'email'}, (email, password, done) => {
+    
+  }));
+}
